de3c2b6d68b6d313ae990252ff049f44a6fd30a2
Mourad EL CADI <mouradface1998@gmail.com>
2023-03-23 16:56:00 +0000

    save

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,18 @@ fn expensive_calculation(_: &i32) {
     std::thread::sleep(std::time::Duration::from_secs(1));
 }
 
-fn main() {
-    let data = vec![1, 2, 3, 4, 5, 6];
-
+fn progress(v: Vec<i32>) {
     let mut i = 0;
 
-    for n in data.iter() {
+    for n in v.iter() {
         println!("{}{}", CLEAR, "*".repeat(i + 1));
         i += 1;
         expensive_calculation(n);
     }
+}
+
+fn main() {
+    let data = vec![1, 2, 3, 4, 5, 6];
+
+    progress(data);
 }
