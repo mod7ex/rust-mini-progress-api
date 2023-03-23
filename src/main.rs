dbb2b36a982e35cd158123c42ae1c2be0c36ff2c
Mourad EL CADI <mouradface1998@gmail.com>
2023-03-23 17:05:32 +0000

    save

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ fn expensive_calculation(_: &i32) {
     std::thread::sleep(std::time::Duration::from_secs(1));
 }
 
-fn progress<T>(v: Vec<T>, f: fn(&T) -> ()) {
+fn progress<T, I>(v: I, f: fn(T) -> ())
+    where I: Iterator<Item = T>
+{
     let mut i = 0;
 
-    for n in v.iter() {
+    for n in v {
         println!("{}{}", CLEAR, "*".repeat(i + 1));
         i += 1;
         f(n);
@@ -17,5 +19,5 @@ fn progress<T>(v: Vec<T>, f: fn(&T) -> ()) {
 fn main() {
     let data = vec![1, 2, 3, 4, 5, 6];
 
-    progress(data, expensive_calculation);
+    progress(data.iter(), expensive_calculation);
 }
